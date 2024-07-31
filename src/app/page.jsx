33a3850b00d429f0af22dae8c4146c08f436e24b
EDUARDO ALVES CASAROTTI <99928133+EduardoCasarotti@users.noreply.github.com>
2024-07-30 22:47:45 -0300

    aula 30/07

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (    
      <div className='container'>
-    <h1>Aula Next</h1>
+    <h1 className={styles.titulo}>Aula Next</h1>
     </div>
   );
 }
