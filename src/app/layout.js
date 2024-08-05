580f56523d674b2229453b2a50ca53772ec19a7b
EDUARDO ALVES CASAROTTI <99928133+EduardoCasarotti@users.noreply.github.com>
2024-08-05 20:37:56 -0300

    aula 05/08

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-// import { Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -12,8 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      {/* <body className={inter.className}>{children}</body> */}
-    <body>{children}</body>
+      { <body className={inter.className}>{children}</body> }
+    {/* <body>{children}</body> */}
     </html>
   );
 }
